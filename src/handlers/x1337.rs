c7ceb29609f17702514a708648546c7b23afa88e
assaf shabili <50052362+AssafShabili@users.noreply.github.com>
2023-02-26 02:03:35 +0200

    Update x1337.rs
    select is a bad crate

--- src/handlers/x1337.rs
+++ src/handlers/x1337.rs
@@ -1,39 +1,37 @@
 use crate::torrent::{Torrent, Torrents, REQWEST_CLIENT};
-use actix_web::{
-    get,
-    web::{Path},
-    HttpResponse,
-};
+use actix_web::{get, web::Path, HttpResponse};
+use select::predicate::{Attr, Class, Name,Not};
 use select::{document::Document, predicate::Predicate};
-use select::predicate::{Attr, Class, Name};
 
-// url of the fitgirl website. ? should I implement a more secure way in order to avoid a fake websites ? i dunno 
+// url of the fitgirl website. ? should I implement a more secure way in order to avoid a fake websites ? i dunno
 pub const URL: &str = "https://1337xx.to/";
 
 async fn extract_info(search_value: &str, page: usize) -> Result<Torrents, reqwest::Error> {
-    
-
     let mut torrents: Vec<Torrent> = Vec::new();
     let html = REQWEST_CLIENT
-        .get(format!("{}search/{}/{}", URL,search_value,page))
+        .get(format!("{}search/{}/{}", URL, search_value, page))
         .send()
         .await?
         .text()
         .await?;
-
 
     //println!("{:?}",html);
     let document = Document::from_read(html.as_bytes()).unwrap();
 
     //TODO: return error if 'search value' is not right
 
-    let table = document.find(
-        Name("table")
-        .descendant(Name("tbody"))
-        .descendant(Name("tr"))).enumerate();
+    let tr = document
+        .find(Name("tr"))
+        .enumerate();
 
-    for tr in table  {
-       println!("{:#?}",tr);
+    for (_,td) in tr {
+        //println!("{:#?}",td.1.html());
+        let name = td.find(Name("td").and(Attr("class","coll-1 name"))).enumerate();
+
+        for f in name {
+            println!("{:?}",f.1);
+        }
+
     }
 
     let ts = Torrents {
