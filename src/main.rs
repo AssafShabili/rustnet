ce15e6f453b382e2988229c5348c4ad30b616ff4
assaf shabili <50052362+AssafShabili@users.noreply.github.com>
2023-03-03 22:26:16 +0200

    last fix to port number

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ async fn main() -> io::Result<()> {
             .service(handlers::x1337::get_torrnets)
             .service(handlers::dodi::get_torrnets)
     })
-    .bind("0.0.0.0:7069")?
+    .bind("0.0.0.0:8088")?
     .run()
     .await
 }
