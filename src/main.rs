55c8d5cd271aaae547ac55d5ac719a30fd1988f6
assaf shabili <assaf6063@gmail.com>
2023-05-27 17:01:03 +0300

    main ip settings fix

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ async fn main() -> io::Result<()> {
             .service(rustnet::handlers::x1337::get_torrnets)
             .service(rustnet::handlers::dodi::get_torrnets)
     })
-    .bind("127.0.0.1:8088")?
+    .bind("0.0.0.0:443")?
     .run()
     .await
 }
