8f421f9aea374b94217e7875a0a936fc8ff585ee
assaf shabili <50052362+AssafShabili@users.noreply.github.com>
2023-06-26 07:22:32 +0300

    added generic traits

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -3,6 +3,37 @@ pub mod dodi;
 pub mod fitgirl;
 pub mod x1337;
 
+use crate::torrent::{Torrent, Torrents};
+use actix_web::{HttpResponse, web::Path};
+use async_trait::async_trait;
+
+
+/// Generic struct to represent all of the torrents Handlers
+pub struct TorrentHandler<'a> {
+    pub search_value: &'a str,
+    pub page: usize,
+}
+
+
+/// Generic trait to represent the extract_info function
+#[async_trait]
+trait ExtractInfo {
+    async fn extract_info(
+        search_value: &str,
+        page: usize,
+    ) -> Result<Torrents, Box<dyn std::error::Error>>;
+}
+
+
+#[async_trait]
+trait GetTorrents {
+    async fn get_torrnets(path: Path<(String, usize)>) -> HttpResponse;
+}
+
+
+
+
+
 
 /// Return a ['String'] type that is the html of the requested url
 pub async fn get_request(url:reqwest::Url) -> Result<String,reqwest::Error> {
